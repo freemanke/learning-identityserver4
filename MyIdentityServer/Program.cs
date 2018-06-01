32b76c25fe3052caeaa689b93fb1761c50cdedd6
Freeman Ke <guang810828@gmail.com>
2018-06-01 16:52:34 +0800

    dotnet run and make post to http://localhost:5001/connect/token will get jwt token

--- MyIdentityServer/Program.cs
+++ MyIdentityServer/Program.cs
@@ -20,6 +20,7 @@ namespace MyIdentityServer
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls(new []{"http://localhost:5001"})
                 .Build();
     }
 }
